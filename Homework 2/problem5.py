dce4d454c74fddd39b7b68e718ae4b76a5021127
AustinB12 <42442591+AustinB12@users.noreply.github.com>
2024-02-19 19:57:25 -0500

    burrrrr

--- Homework 2/problem5.py
+++ Homework 2/problem5.py
@@ -1,20 +1,24 @@
 import random
 import time
 from tqdm import tqdm
-from quicksort_median_of_3_pivot import quicksort_median_pivot
 from quick_sort_rpivot import quicksort_rpivot
 import matplotlib.pyplot as plt
+from statistics import median
+import sys
 
+sys.setrecursionlimit(10000)
 
 def main():
-    sizes = range(100, 10000, 500)
+    # sizes = range(100, 10000, 500)
+    sizes = range(100, 5000, 500)
 
     qs_random_pivot_times = []
     qs_rpivot_times = []
     qs_median_three_times = [] 
 
     for size in tqdm(sizes):
-        A = [random.randint(-1000, 1000) for i in range(size)]
+        # A = [random.randint(-1000, 1000) for i in range(size)]
+        A = [i for i in range(size)]
         
         qs_random_pivot_time = 0
         qs_rpivot_time = 0
@@ -37,10 +41,9 @@ def main():
 
             B = list(A)
             x = [B[0],B[len(B)//2],B[-1]]
-            x.sort()
-            y = x[1]
+            pivot = median(x)
             start = time.perf_counter()
-            quicksort_median_pivot(B, y, len(B) - 1)
+            quicksort_rpivot(B, 0, pivot)
             end = time.perf_counter()
             qs_median_three_time += end - start
 
@@ -49,9 +52,9 @@ def main():
         qs_rpivot_times.append(qs_rpivot_time / 5)
         qs_median_three_times.append(qs_median_three_time / 5)
     
-    plt.plot(sizes, qs_random_pivot_times, label='Merge Sort')
-    plt.plot(sizes, qs_rpivot_times, label='Heap Sort')
-    plt.plot(sizes, qs_median_three_times, label='Quick Sort Rightmost Pivot')
+    # plt.plot(sizes, qs_random_pivot_times, label='Merge Sort')
+    plt.plot(sizes, qs_rpivot_times, label='Quick Sort Rightmost Pivot')
+    plt.plot(sizes, qs_median_three_times, label='Quick Sort Median of 3 Pivot')
     plt.xlabel('Size of Array')
     plt.ylabel('Execution Time (s)')
     plt.title('Execution Time of Merge Sort, Heap Sort, and Quick Sort Median of 3 Pivot')
